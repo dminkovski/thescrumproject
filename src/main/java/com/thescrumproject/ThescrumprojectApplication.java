72d61cd92987a96ea627a42d3c87f15728e8cfb3
DM05744 <david.minkovski@devoteam.com>
2022-11-22 12:10:27 +0100

    Initial commit

---
+++ src/main/java/com/thescrumproject/ThescrumprojectApplication.java
@@ -0,0 +1,13 @@
+package com.thescrumproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThescrumprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThescrumprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thescrumproject/ThescrumprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thescrumproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThescrumprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
